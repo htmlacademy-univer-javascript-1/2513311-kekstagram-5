c086eb41d1c7eb2b892f791feca173c43dd6aff2
Keks <keks@htmlacademy.ru>
2024-10-24 17:32:02 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -3,15 +3,36 @@ function getRandomInt(min, max) {
   return min + Math.random() * (max - min + 1) >> 0;
 }
 
-const fixedMessage = 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.';
-const fixedName = 'Анна';
+function getRandomMessage() {
+  const messages = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
+  const messageCount = getRandomInt(1, 2);
+  const randomMessages = [];
+  for (let i = 0; i < messageCount; i++) {
+    const randomIndex = getRandomInt(0, messages.length - 1);
+    randomMessages.push(messages[randomIndex]);
+  }
+  return randomMessages.join(' ');
+}
+
+function getRandomName() {
+  const names = ['Артём', 'Анна', 'Дмитрий', 'Елена', 'Иван', 'Мария', 'Николай', 'Ольга', 'Пётр', 'Светлана'];
+  const randomIndex = getRandomInt(0, names.length - 1);
+  return names[randomIndex];
+}
 
 function generateComment(id) {
   return {
     id: id,
     avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
-    message: fixedMessage,
-    name: fixedName
+    message: getRandomMessage(),
+    name: getRandomName()
   };
 }
 
