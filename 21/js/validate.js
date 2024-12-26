c45e9a0dfcb8d4a1964d8ddf9f580a79b39f77a2
Keks <keks@htmlacademy.ru>
2024-12-26 19:24:53 +0300

    :heavy_check_mark: Сборка #21

---
+++ 21/js/validate.js
@@ -0,0 +1,129 @@
+import { resetScale } from './scale.js';
+import { init as initEffect, reset as resetEffect} from './effects.js';
+
+const body = document.querySelector('body');
+const form = document.querySelector('.img-upload__form');
+const overlay = form.querySelector('.img-upload__overlay');
+const cancelButton = form.querySelector('.img-upload__cancel');
+const fileField = form.querySelector('.img-upload__input');
+const commentField = form.querySelector('.text_description');
+const submitButton = form.querySelector('.img-upload__submit');
+const hashtagField = form.querySelector('.text__hashtags');
+const photoPreview = form.querySelector('.img-upload__preview img');
+const effectsPreviews = form.querySelectorAll('.effects__preview');
+
+const MAX_HASHTAG_COUNT = 5;
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
+const ErrorText = {
+  INVALID_COUNT: `Максимум ${MAX_HASHTAG_COUNT} хештегов`,
+  NOT_UNIQUE: 'Хештеги должны быть уникальными',
+  INVALID_PATTERN: 'Неправильный хештег',
+};
+
+const SubmitButtonText = {
+  IDLE: 'Опубликовать',
+  SUBMITTING: 'Отправляю...',
+};
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error',
+});
+
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const hideModal = () => {
+  form.reset();
+  resetScale();
+  resetEffect();
+  pristine.reset();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const toggleSubmitButton = (isDisabled) => {
+  submitButton.disabled = isDisabled;
+  submitButton.textContent = isDisabled ? SubmitButtonText.SUBMITTING : SubmitButtonText.IDLE;
+};
+
+const isErrorMessageShown = () => Boolean(document.querySelector('.error'));
+
+const isTextFieldFocused = () => document.activeElement === hashtagField || document.activeElement === commentField;
+
+const isValidType = (file) => {
+  const fileName = file.name.toLowerCase();
+  return FILE_TYPES.some((it) => fileName.endsWith(it));
+};
+
+const normalizeTags = (tagString) => tagString.trim().split(' ').filter((tag) => Boolean(tag.length));
+
+const hasValidTags = (value) => normalizeTags(value).every((tag) => VALID_SYMBOLS.test(tag));
+
+const hasValidCount = (value) => normalizeTags(value).length <= MAX_HASHTAG_COUNT;
+
+const hasUniqueTags = (value) => {
+  const lowerCaseTags = normalizeTags(value).map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape' && !isTextFieldFocused() && !isErrorMessageShown()) {
+    evt.preventDefault();
+    hideModal();
+  }
+}
+
+const onCancelButtonClick = () => {
+  hideModal();
+};
+
+const onFileInputChange = () => {
+  const file = fileField.files[0];
+
+  if (file && isValidType(file)) {
+    resetEffect();
+    photoPreview.src = URL.createObjectURL(file);
+    effectsPreviews.forEach((preview) => {
+      preview.style.backgroundImage = `url('${photoPreview.src}')`;
+    });
+  }
+  showModal();
+};
+
+const setOnFormSubmit = (callback) => {
+  form.addEventListener('submit', async (evt) => {
+    evt.preventDefault();
+    const isValid = pristine.validate();
+
+    if (isValid) {
+      toggleSubmitButton(true);
+      await callback(new FormData(form));
+      toggleSubmitButton();
+    }
+  });
+};
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
+fileField.addEventListener('change', onFileInputChange);
+cancelButton.addEventListener('click', onCancelButtonClick);
+form.addEventListener('submit', onFormSubmit);
+initEffect();
+
+pristine.addValidator(hashtagField, hasValidCount, ErrorText.INVALID_COUNT, 3, true);
+
+pristine.addValidator(hashtagField, hasUniqueTags, ErrorText.NOT_UNIQUE, 1, true);
+
+pristine.addValidator(hashtagField, hasValidTags, ErrorText.INVALID_PATTERN, 2, true);
+
+export{hideModal, setOnFormSubmit};
---
+++ 21/js/view-big-picture.js
@@ -0,0 +1,93 @@
+const COMMENTS_PER_PORTION = 5;
+
+const bigPictureElement = document.querySelector('.big-picture');
+const commentShownCountElement = bigPictureElement.querySelector('.comments-shown-count');
+const commentCountElement = bigPictureElement.querySelector('.comments-count');
+const commentListElement = bigPictureElement.querySelector('.social__comments');
+const commentLoadElement = bigPictureElement.querySelector('.comments-loader');
+const bodyElement = document.querySelector('body');
+const cancelElement = bigPictureElement.querySelector('.big-picture__cancel');
+const commentElementTemplate = document.querySelector('.social__comment');
+
+const createComment = function({ avatar, name, message }) {
+  const comment = commentElementTemplate.cloneNode(true);
+
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+
+let commentsShown = 0;
+let comments = [];
+
+const renderComments = function() {
+  commentsShown += COMMENTS_PER_PORTION;
+
+  if (commentsShown >= comments.length) {
+    commentLoadElement.classList.add('hidden');
+    commentsShown = comments.length;
+  } else {
+    commentLoadElement.classList.remove('hidden');
+  }
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < commentsShown; i++) {
+    const comment = createComment(comments[i]);
+    fragment.append(comment);
+  }
+
+  commentListElement.innerHTML = '';
+  commentListElement.append(fragment);
+  commentShownCountElement.textContent = commentsShown;
+  commentCountElement.textContent = comments.length;
+};
+
+const hidePicture = function() {
+  bigPictureElement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  commentsShown = 0;
+};
+
+function onDocumentKeydown(evt) {
+  if(evt.key === 'Escape') {
+    evt.preventDefault();
+    hidePicture();
+  }
+}
+
+const onCancelButtonClick = function() {
+  hidePicture();
+};
+
+const onCommentLoadClick = function() {
+  renderComments();
+};
+
+const renderPictureDetails = function({ url, likes, description }) {
+  bigPictureElement.querySelector('.big-picture__img img').src = url;
+  bigPictureElement.querySelector('.big-picture__img img').alt = description;
+  bigPictureElement.querySelector('.likes-count').textContent = likes;
+  bigPictureElement.querySelector('.social__caption').textContent = description;
+};
+
+const showBigPicture = function(data) {
+  bigPictureElement.classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+  commentLoadElement.classList.add('hidden');
+  // commentCountElement.classList.add('hidden');
+  document.addEventListener('keydown', onDocumentKeydown);
+
+  renderPictureDetails(data);
+  comments = data.comments;
+  if (comments.length > 0) {
+    renderComments();
+  }
+};
+
+cancelElement.addEventListener('click', onCancelButtonClick);
+commentLoadElement.addEventListener('click', onCommentLoadClick);
+
+export { showBigPicture };
